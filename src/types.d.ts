8988321cae2fca7956d04dcb4535a4c6e658aaf1
NataliiaBumbu <130250304+NataliiaBumbu@users.noreply.github.com>
2024-10-31 13:48:22 +0200

    hw

---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+declare module './components/SearchBar/SearchBar';
+declare module './components/ImageGallery/ImageGallery';
+declare module './components/ImageModal/ImageModal';
+declare module './components/Loader/Loader';
+declare module './components/ErrorMessage/ErrorMessage';
+declare module './components/LoadMoreBtn/LoadMoreBtn';
